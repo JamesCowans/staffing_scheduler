2fcdbdbfd8f877443c59543074b73e5ba80f2059
JamesCowans <>
2023-10-14 14:59:06 +0000

    Add data validation function to check the correct number of values have been added

--- run.py
+++ run.py
@@ -15,29 +15,43 @@ SHEET = GSPREAD_CLIENT.open('dc_productivity')
 department_prod = SHEET.worksheet('department_prod')
 
 data = department_prod.get_all_values()
-print(data)
+
 
 def daily_volume_general():
     """
     Asks the user to input the unit volume of delivery colleagues will be expected to process
     """
 
-    print("Please input the expected general merchandise delivery volumes: ")
 
-    general_vol = input("Please enter volumes for general merchandise deliveries for the the week.")
-    print(f'{general_vol}')
+    general_vol = input("Please enter volumes for general merchandise deliveries for the the week: ")
+
+    daily_vol_gen = general_vol.split(",")
+    validate_data(daily_vol_gen)
+
 def daily_volume_beauty():
     """
     Asks the user to input the unit volume of beauty specific delivery to be processed, this 
     follows a different path to general delivery.
     """
 
-    print("Please input the expected beauty product delivery volumes: ")
-    beauty_vol = input("Please enter volumes for beauty deliveries for the the week....\n")
-    print(f'{beauty_vol}')
+    
+    beauty_vol = input("Please enter volumes for beauty deliveries for the the week: ")
+ 
+    daily_vol_bty = beauty_vol.split(",")
+    validate_data(daily_vol_bty)
 
 
-
+def validate_data(values):
+    """
+    Inside the try, strings converted to integers and ValueError raised if the incorrect number of data
+    is inputted or cannot be converted
+    """
+    try:
+        if len(values) !=5:
+            raise ValueError(
+        f"The number of values entered must total 5, you entered{values}")
+    except ValueError as e:
+        print(f"Invalid date: {e}, please try again.\n")
 
 
 
@@ -51,6 +65,7 @@ def main():
     print("Example: 14543, 34322, 23253, 23242, 23232")
     daily_volume_general()
     daily_volume_beauty()
+    
 
 
 main()
