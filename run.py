17a754c32f67a979a052791947603d827d97ad30
JamesCowans <>
2023-10-16 20:51:36 +0000

    name variable added for test

--- run.py
+++ run.py
@@ -279,5 +279,5 @@ print("Welcome to the DC staff schedule assistant...\n")
 print("Data must be all 5 days, each day seperated by a comma..\n")
 print("Example: 14543, 34322, 23253, 23242, 23232")
     
-
+name=input("Please enter your name:\n")
 main()
